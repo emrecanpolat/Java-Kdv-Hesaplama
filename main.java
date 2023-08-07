e5d76622fcfdc4fce28aa57a20568c9511f820e5
emrecanpolat <35192029+emrecanpolat@users.noreply.github.com>
2023-08-07 13:22:30 +0300

    Update main.java

--- main.java
+++ main.java
@@ -11,13 +11,25 @@ public class Main {
         System.out.print("Ürüm Fiyatını Giriniz:");
         urunFiyat = input.nextInt();  // Read user input
 
-        kdvYuzdesi = 0.18;
+        if (urunFiyat<=1000){
+            kdvYuzdesi = 0.18;
 
-        kdvliUrunFiyati = urunFiyat+(urunFiyat*kdvYuzdesi);
-        kdvTutari = kdvliUrunFiyati-urunFiyat;
+            kdvliUrunFiyati = urunFiyat+(urunFiyat*kdvYuzdesi);
+            kdvTutari = kdvliUrunFiyati-urunFiyat;
 
-        System.out.println("KDV'li ürün fiyatı: "+ kdvliUrunFiyati);
-        System.out.println("KDV Tutarı: "+ kdvTutari);
+            System.out.println("KDV'li ürün fiyatı: "+ kdvliUrunFiyati);
+            System.out.println("KDV Tutarı: "+ kdvTutari);
+        }else {
+            kdvYuzdesi = 0.08;
+
+            kdvliUrunFiyati = urunFiyat+(urunFiyat*kdvYuzdesi);
+            kdvTutari = kdvliUrunFiyati-urunFiyat;
+
+            System.out.println("KDV'li ürün fiyatı: "+ kdvliUrunFiyati);
+            System.out.println("KDV Tutarı: "+ kdvTutari);
+        }
+
+
 
     }
 }
